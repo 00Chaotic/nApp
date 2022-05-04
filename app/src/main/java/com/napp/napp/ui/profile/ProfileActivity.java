5322783298afcfd9d1e2401f348df54d47c99182
Jaffar Rizvi <J.Rizvi@outlook.com>
2022-05-04 14:34:08 +1000

    Napp 59 view profile (#6)
    * NAPP-50: Update and improve logic to add auth middleware
    
    * NAPP-59: Initial profile page code

---
+++ app/src/main/java/com/napp/napp/ui/profile/ProfileActivity.java
@@ -0,0 +1,40 @@
+package com.napp.napp.ui.profile;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.napp.napp.MyApplication;
+import com.napp.napp.R;
+import com.napp.napp.data.model.User;
+import com.napp.napp.databinding.ActivityProfileBinding;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private MyApplication myApplication;
+
+    private ActivityProfileBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myApplication = (MyApplication) this.getApplication();
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        final TextView nameTextView = binding.nameTextView;
+        final TextView phoneNumberTextView = binding.phoneNumberTextView;
+        final TextView locationTextView = binding.locationTextView;
+
+        // Retrieve and de-serialise user info
+        String userJson = myApplication.getApplicationSharedPreferences().getString(myApplication.getString(R.string.preference_user), null);
+        User user = new Gson().fromJson(userJson, User.class);
+
+        nameTextView.setText(user.getDisplayName());
+        phoneNumberTextView.setText(user.getPhoneNumber());
+        // TODO add location once it is available from API
+    }
+}
