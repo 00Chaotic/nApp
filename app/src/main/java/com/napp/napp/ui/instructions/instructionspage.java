0125a0066e1b98972c79fa09dd0117fff3ac4476
jasminadang <jasminadang@gmail.com>
2022-05-28 18:36:59 +1000

    instruction page

---
+++ app/src/main/java/com/napp/napp/ui/instructions/instructionspage.java
@@ -0,0 +1,17 @@
+package com.napp.napp.ui.instructions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.napp.napp.R;
+
+public class instructionspage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructionspage);
+
+    }
+}
