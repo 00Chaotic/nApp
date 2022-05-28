aae90940860b6fcea1a15a3d1f2b765153e480dd
jasminadang <48747159+jasminadang@users.noreply.github.com>
2022-05-28 12:04:57 +1000

    booking and welcome page (#10)
    * booking and welcome page
    
    * hopefully these changes fix the problem - please let me know if there's any other problem

---
+++ app/src/main/java/com/napp/napp/ui/bookingpage/bookingpage.java
@@ -0,0 +1,17 @@
+package com.napp.napp.ui.bookingpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.napp.napp.R;
+
+public class bookingpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookingpage);
+
+    }
+}
---
+++ app/src/main/java/com/napp/napp/ui/welcomepage/welcomepage.java
@@ -0,0 +1,16 @@
+package com.napp.napp.ui.welcomepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.napp.napp.R;
+
+public class welcomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomepage);
+    }
+}
