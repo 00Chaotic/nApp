75aeb02b4de22edc09dd7821ddb1d1de6908f17b
David Bolis <davidmaged1@hotmail.com>
2022-06-01 14:42:21 +1000

    UI Completion
    Every UI is reviewed and completed. There will be another push after this confirming the segways are also working.

--- app/src/main/java/com/napp/napp/specialinstruction.java
+++ app/src/main/java/com/napp/napp/specialinstruction.java
@@ -1,14 +1,26 @@
 package com.napp.napp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class specialinstruction extends AppCompatActivity {
+
+    Spinner spinnernappies = findViewById(R.id.spinner_nappies);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specialinstruction);
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnernappies.setAdapter(adapter);
     }
 }
