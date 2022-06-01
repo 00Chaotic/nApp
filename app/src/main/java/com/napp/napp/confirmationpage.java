7404765f13e3f003762b05acc5985d66906b1ec9
David Bolis <davidmaged1@hotmail.com>
2022-06-01 13:31:12 +1000

    UICompletion
    Do not merge, something must be fixed first

---
+++ app/src/main/java/com/napp/napp/confirmationpage.java
@@ -0,0 +1,14 @@
+package com.napp.napp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class confirmationpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmationpage);
+    }
+}
